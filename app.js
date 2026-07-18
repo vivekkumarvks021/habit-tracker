6bc9151dfd5580b23fa0173563f3e7b50faff377
vivekkumarvks021 <vivekkumarvks021@gmail.com>
2026-07-18 13:37:20 +0530

    Initial Express & EJS setup

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+const express = require("express");
+const connectDB = require("./config/mongoose");
+
+const app = express();
+
+connectDB();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
